6320c5edb9ff079ee1f87d568d09dabffcf54bef
Paul Amen <paul6am@hotmail.com>
2024-09-01 11:39:44 -0500

    feat: Actualizar servidor para servir archivos estáticos desde el directorio raíz

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Sirve archivos estáticos desde el directorio 'public'
-app.use(express.static('public'));
+// Sirve archivos estáticos desde el directorio actual (raíz)
+app.use(express.static(__dirname));
 
 // Ruta para la página principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Inicia el servidor
